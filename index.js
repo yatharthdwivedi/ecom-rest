0e8789c4fea079051148eb919d4ad1cfbebf38cd
yatharthdwivedi <yatharthdwivedi1@gmail.com>
2022-07-22 01:04:37 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const userRouter = require('./routers/user')
+const itemRouter =require('./routers/item')
+const cartRouter = require('./routers/cart')
+require('./mongoose/db')
+
+const port = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+app.use(userRouter)
+app.use(itemRouter)
+app.use(cartRouter)
+
+
+app.listen(port, () => {
+    console.log('server listening on port ' + port)
+})
---
+++ mongoose/db.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO, {
+useNewUrlParser: true,
+})
